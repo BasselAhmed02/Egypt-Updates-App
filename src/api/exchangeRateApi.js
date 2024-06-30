aafe228b0d147cb0f39d71189f559bed8b6ee2d1
BasselAhmed02 <Bassel.M.Ahmed02@gmail.com>
2024-06-30 19:13:47 +0300

    Fixing the News API

--- src/api/exchangeRateApi.js
+++ src/api/exchangeRateApi.js
@@ -1,9 +1,7 @@
-const API_KEY = "YOUR_EXCHANGE_RATE_API_KEY";
 const BASE_URL = "https://api.exchangerate-api.com/v4/latest";
 
 export const fetchExchangeRate = async () => {
   const response = await fetch(`${BASE_URL}/USD`);
   const data = await response.json();
-  console.log(data.rates.EGP);
   return data.rates.EGP;
 };
